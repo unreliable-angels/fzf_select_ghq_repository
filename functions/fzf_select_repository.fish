4f0ddd6d5773fa68db08d25d01b314f21f7b8297
Hirofumi Wakasugi <baenej@gmail.com>
2019-11-18 15:12:24 +0900

    Fix function name

--- functions/fzf_select_repository.fish
+++
@@ -1,8 +0,0 @@
-function fzf_select_ghq_repository -d "fzf source to cd to repository using ghq"
-    ghq list -p | fzf | read repository
-
-    if test -n "$repository"
-        commandline "cd $repository"
-        commandline -f execute
-    end
-end
